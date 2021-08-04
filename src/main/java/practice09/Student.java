ec53a5285857401a185deda6e42e64a3698861cd
OnceRobert <robpurugganan@gmail.com>
2021-08-04 22:47:17 +0800

    [OOPPractice][Roberto] feat:Practice09

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,25 @@
 package practice09;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+    public Student(int ID, String name, int age, Klass klass){
+        super(ID, name,age);
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        if (klass.getLeader() == this.getKlass().getLeader())
+            return super.introduce() + " I am a Student. I am Leader of Class " + klass.getNumber() + ".";
+        else
+            return super.introduce() + " I am a Student. I am at Class " + klass.getNumber() + ".";
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,66 @@
 package practice09;
 
-public class Teacher {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Teacher extends Person {
+    public Klass klass;
+    public List<Klass> klasses = new ArrayList<Klass>();
+    public Teacher(int ID, String name, int age, Klass klass){
+        super(ID,name,age);
+        this.klass = klass;
+    }
+
+    public Teacher(int ID, String name, int age){
+        super(ID,name,age);
+        klass = null;
+    }
+
+    public Teacher(int id, String tom, int age, LinkedList<Klass> linkedList) {
+        super(id, tom,age);
+        klasses = linkedList;
+
+    }
+
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        if(klasses==null)
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        else {
+            String klassesTeach = String.format("%d", klasses.get(0).getNumber());
+            for(int i = 1; i<klasses.size();i++){
+                klassesTeach += ", " + klasses.get(i).getNumber();
+            }
+            return super.introduce() + " I am a Teacher. I teach Class " + klassesTeach + ".";
+        }
+    }
+
+    public String introduceWith(Student student){
+        String introduction = super.introduce();
+        if(student.getKlass()==this.klass)
+            return String.format("%s I am a Teacher. I teach %s",introduction,student.getName());
+        else
+            return String.format("%s I am a Teacher. I don't teach %s",introduction,student.getName());
+    }
+
+    public List<Klass> getClasses() {
+        return klasses;
+    }
+
+    public boolean isTeaching(Student jerry) {
+        if (klasses.contains(jerry.getKlass())){
+            return true;
+        }
+        else
+            return false;
+    }
 }
